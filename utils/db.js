8e9048fdc807b0ffdc9a0786d4511ab9d7dfec64
SamZec <samzec4@gmail.com>
2023-12-02 16:56:56 +0000

    1. MongoDB utils

--- utils/db.js
+++ utils/db.js
@@ -2,10 +2,10 @@ import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || '27017';
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    const uri = `mongodb://${this.host}:${this.port}/${this.database}`;
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const uri = `mongodb://${host}:${port}/${database}`;
     this.client = new MongoClient(uri, { useUnifiedTopology: true });
     this.client.connect();
   }
@@ -15,15 +15,11 @@ class DBClient {
   }
 
   async nbUsers() {
-    await this.client;
-    const collection = this.client.db().collection('users');
-    return await collection.countDocuments();
+    return this.client.db().collection('users').countDocuments();
   }
 
   async nbFiles() {
-    await this.client;
-    const collection = this.client.db().collection('files');
-    return await collection.countDocuments();
+    return this.client.db().collection('files').countDocuments();
   }
 };
 
