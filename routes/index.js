4237e5b7f81c6731d7f3d9570b64204ae6d4a378
SamZec <samzec4@gmail.com>
2023-12-03 11:44:17 +0000

    3. Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 // eslint-disable-next-line no-unused-vars
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-import UserController from '../controllers/UserController';
+import UsersController from '../controllers/UsersController';
 
 const Routes = Router();
 
 Routes.get('/status', AppController.getStatus);
 Routes.get('/stats', AppController.getStats);
-Routes.post('/users', UserController.postNew);
+Routes.post('/users', UsersController.postNew);
 
 module.exports = Routes;
