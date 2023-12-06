afcdbf5a125435d06ddd015c5ba20571563788d1
SamZec <samzec4@gmail.com>
2023-12-06 16:25:44 +0000

    Uodate

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,16 +1,16 @@
-//import { app } from '../server';
+// import { app } from '../server';
 import { redisClient } from '../utils/redis';
 import { dbClient } from '../utils/db';
 
 module.exports = class AppController {
   static getStatus(req, res) {
-    res.status(200).json({redis: redisClient.isAlive(), db: dbClient.isAlive()});
+    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
   static getStats(req, res) {
     Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
       .then(([users, files]) => {
-        res.status(200).json({"users": users, "files": files});
+        res.status(200).json({ users, files });
       });
   }
 };
